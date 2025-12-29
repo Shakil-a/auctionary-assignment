b83f5d7fec495c61913f1101a9581bc98cdfcea7
shakil ahmed <ahmed1shakil@gmail.com>
2025-12-29 13:36:59 +0000

    Sql to to get item details

--- app/models/core.server.models.js
+++ app/models/core.server.models.js
@@ -37,8 +37,29 @@ const getItemByItemId = (item_id, callback) => {
 }
 
 const getItemDetailsByItemId = (item_id, callback) => {
-    const sql = 'SELECT items.*, users.first_name, users.last_name FROM items JOIN users ON items.creator_id = users.user_id WHERE items.item_id = ?';
-    db.get(sql, [item_id], (err, itemDetails) => {
+    const sql = `SELECT 
+                items.*,
+                creator.first_name AS creator_first_name,
+                creator.last_name AS creator_last_name,
+                latest_bid.amount AS current_bid,
+                bidder.user_id AS current_bid_user_id,
+                bidder.first_name AS current_bid_first_name,
+                bidder.last_name AS current_bid_last_name
+            FROM items
+            JOIN users AS creator
+                ON items.creator_id = creator.user_id
+            LEFT JOIN (
+                SELECT *
+                FROM bids
+                WHERE item_id = ?
+                ORDER BY timestamp DESC
+                LIMIT 1
+            ) AS latest_bid
+                ON latest_bid.item_id = items.item_id
+            LEFT JOIN users AS bidder
+                ON latest_bid.user_id = bidder.user_id
+            WHERE items.item_id = ?`;
+    db.get(sql, [item_id, item_id], (err, itemDetails) => {
         if (err) return callback(err);
         return callback(null, itemDetails);
     });
